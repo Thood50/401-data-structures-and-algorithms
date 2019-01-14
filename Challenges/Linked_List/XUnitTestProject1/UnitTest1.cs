6109aa34122b13e482c1229c5254898dadb48854
Tyler Hood <tyler.r.hood@gmail.com>
2019-01-14 11:23:43 -0800

    Undid some of my suggestions from McGoogle

--- Challenges/Linked_List/XUnitTestProject1/UnitTest1.cs
+++ Challenges/Linked_List/XUnitTestProject1/UnitTest1.cs
@@ -7,17 +7,14 @@ namespace XUnitTestProject1
 {
     public class UnitTest1
     {
-        private const bool V = false;
+        
 
         [Fact]
         public void Test1()
         {
-            Assert.Equal(V, Includes(4));
+            Assert.Equal(true, Includes(4));
         }
 
-        private IEnumerable<object> Includes(int v)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
