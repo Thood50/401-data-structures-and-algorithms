f6f340c75ada794839e9bcc66786ad48484493c6
Tyler Hood <tyler.r.hood@gmail.com>
2019-01-21 08:30:22 -0800

    Coded out the Queue class

--- Data Structures/StackAndQueue/StackAndQueue/Classes/Queue.cs
+++ Data Structures/StackAndQueue/StackAndQueue/Classes/Queue.cs
@@ -1,10 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Linked_List;
+using ll_kth_from_end;
+using LL_merge;
 
 namespace StackAndQueue.Classes
 {
-    class Queue
+    public class Queue
     {
+        public Node Front { get; set; }
+
+        public Node Rear { get; set; }
+
+        public Queue (Node node)
+        {
+            Front = node;
+            Rear = node;
+        }
+
+        /// <summary>
+        /// Adds a new node to end of queue
+        /// </summary>
+        /// <param name="value"></param>
+        public void Enqueue (int value)
+        {
+            Node node = new Node(value);
+            Rear.Next = node;
+            Rear = node;
+        }
+
+        /// <summary>
+        /// Grabs front of queue and removes it from the queue
+        /// </summary>
+        /// <returns>returns what was originally the front of queue</returns>
+        public Node Dequeue ()
+        {
+            Node temp = Front;
+            Front = Front.Next;
+            temp.Next = null;
+            return temp;
+        }
+
+        /// <summary>
+        /// Lets you look at the front of the queue
+        /// </summary>
+        /// <returns>returns whatever is at front of queue</returns>
+        public Node Peek ()
+        {
+            return Front;
+        }
     }
 }
