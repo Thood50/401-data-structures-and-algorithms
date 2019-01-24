cd0b9b42f313047d6366e8acb8e9b81a7af68146
Tyler Hood <tyler.r.hood@gmail.com>
2019-01-24 14:02:35 -0800

    Finished code, test, and readme

---
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Classes/CharStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiBracketValidation.Classes
+{
+    public class CharStack
+    {
+        public CharNode Top { get; set; }
+
+        public CharStack()
+        {
+            Top = null;
+        }
+
+        public CharStack(CharNode node)
+        {
+            Top = node;
+        }
+
+        /// <summary>
+        /// Adds new node to top of stack
+        /// </summary>
+        /// <param name="value"></param>
+        public void Push(char value)
+        {
+            CharNode node = new CharNode(value);
+            node.Next = Top;
+            Top = node;
+        }
+
+        /// <summary>
+        /// Removes top node from stack
+        /// </summary>
+        /// <returns>returns the node that was on top of stack</returns>
+        public CharNode Pop()
+        {
+            CharNode temp = Top;
+            Top = Top.Next;
+            temp.Next = null;
+            return temp;
+        }
+
+        /// <summary>
+        /// Allows you to view top of stack
+        /// </summary>
+        /// <returns>returns the node at top of stack</returns>
+        public CharNode Peek()
+        {
+            return Top;
+        }
+    }
+}
---
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using MultiBracketValidation.Classes;
+
+namespace MultiBracketValidation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        /// <summary>
+        /// This method takes in a string and checks for proper Bracket notation
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>returns false if proper bracket notation is not met, return true if it has been met</returns>
+        public static bool MBV(string str)
+        {
+            CharStack stack = new CharStack();
+            foreach (char c in str)
+            {
+                string check = c.ToString();
+                if (check == "(" || check == "[" || check == "{")
+                {
+                    stack.Push(c);
+                }
+                if (check == ")")
+                {
+                    if (stack.Pop().Value.ToString() != "(")
+                    {
+                        return false;
+                    }
+                }
+                if (check == "]")
+                {
+                    if (stack.Pop().Value.ToString() != "[")
+                    {
+                        return false;
+                    }
+                }
+                if (check == "}")
+                {
+                    if (stack.Pop().Value.ToString() != "{")
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (stack.Peek() != null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
